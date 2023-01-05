a108cac38bce60fc56db0c3d2d4ee48a4cc7b62a
Zinostick <99473643+Zinostick@users.noreply.github.com>
2023-01-06 00:47:38 +0100

    Create 0-add.py

---
+++ 0x00-python_variable_annotations/0-add.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+"""
+type-annoted function that adds two numbers
+"""
+
+
+def add(a: float, b: float) -> float:
+    """ Takes two floates and adds them """
+    return a + b

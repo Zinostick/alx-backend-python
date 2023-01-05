31de84baa1a5b445b059baac17297652e9e89eac
Zinostick <99473643+Zinostick@users.noreply.github.com>
2023-01-06 00:49:20 +0100

    Create 3-to_str.py

---
+++ 0x00-python_variable_annotations/3-to_str.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""
+A type annotated function that returns a string
+"""
+
+
+def to_str(n: float) -> str:
+    """
+    Returns the string representation of the argument
+    """
+    return str(n)

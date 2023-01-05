7a122de461dfde348a756a9ddaca6638c2bad19d
Zinostick <99473643+Zinostick@users.noreply.github.com>
2023-01-06 00:48:13 +0100

    Create 1-concat.py

---
+++ 0x00-python_variable_annotations/1-concat.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""
+A type-annotated function that takes two strings
+"""
+
+
+def concat(str1: str, str2: str) -> str:
+    """
+    Takes two stringes and concatenates them
+    """
+    return(str1 + str2)
